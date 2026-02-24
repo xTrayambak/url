86a61bc1165a1d15140c08b012949927f7828959
xTrayambak <xtrayambak@gmail.com>
2026-02-24 11:52:51 +0530

    fix: search: don't try processing 4 bytes per iteration
    That's an eldritch horror that yields no tangible benefit. I think I
    drank too much of the "process as much as once" kool-aid. :P

--- src/url/search.nim
+++ src/url/search.nim
@@ -45,26 +45,6 @@ func findInsensitive*(view: views.StringView, needle: char): int =
 
       i += cap
 
-    while i + 4 < view.len:
-      let
-        j = i + 1
-        k = i + 2
-        l = i + 3
-
-      if view[i] == needle:
-        return cast[int](i)
-
-      if view[j] == needle:
-        return cast[int](j)
-
-      if view[k] == needle:
-        return cast[int](k)
-
-      if view[l] == needle:
-        return cast[int](l)
-
-      i += 4
-
     while i < view.len:
       if view[i] == needle:
         return cast[int](i)
