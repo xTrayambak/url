edf3ac3d763495cb7d5d9b37483f90a57b0770be
xTrayambak <xtrayambak@gmail.com>
2026-02-24 11:52:06 +0530

    add: new test suite for fast-search path

--- tests/tfastsearch.nim
+++ tests/tfastsearch.nim
@@ -1,7 +1,11 @@
 ## Testing the fast-path for case insensitive view search
 ##
 ## Copyright (C) 2026 Trayambak Rai (xtrayambak@disroot.org)
+import std/unittest
 import pkg/url/[views, search]
 
-let x = toStringView("hello there. i am totally not losing my mind!")
-echo findInsensitive(x, 'z')
+test "case insensitive search fast-path":
+  let x = toStringView("hello there. i am totally not losing my mind!")
+  check(findInsensitive(x, 'z') == -1)
+  check(findInsensitive(x, '!') == int32(x.len - 1))
+  check(findInsensitive(x, 'h') == 0)
